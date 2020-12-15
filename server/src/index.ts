fcb7df646d39ef02bcf1a977b6b57fc34ce65b73
BaileyDay <bailey.j.day@gmail.com>
2020-12-15 12:32:40 -0500

    Added Typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
-const express = require('express');
+import express from 'express'
 const app = express();
 const port = 9000;
+
+const one: number = 1;
+const two: number = 2;
 
 app.get("/", (req, res) => res.send("Hello Again"));
 
